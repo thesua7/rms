d4009b64d553943916a4a27049dad46a2e0925e1
ahamed-kashif <ahamed.kashif@northsouth.edu>
2020-11-09 07:22:01 +0600

    app.layout done

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,6 +22,7 @@
     <link href="{{asset('css/icons.min.css')}}" rel="stylesheet" type="text/css" />
     <!-- App Css-->
     <link href="{{asset('css/app.min.css')}}" id="app-style" rel="stylesheet" type="text/css" />
+    @yield('css')
 
 </head>
 
@@ -45,11 +46,8 @@
             <div class="container-fluid">
 
                 <!-- start page title -->
-                @component('components.page-title')
-                @endcomponent
+                @include('components.page-title')
                 <!-- end page title -->
-
-
                 <div class="checkout-tabs">
                     <div class="row">
                         <div class="col-lg-12">
@@ -72,11 +70,11 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-sm-6">
-                        <script>document.write(new Date().getFullYear())</script> © Skote.
+                        <script>document.write(new Date().getFullYear())</script> © maroon.lab.
                     </div>
                     <div class="col-sm-6">
                         <div class="text-sm-right d-none d-sm-block">
-                            Design & Develop by Themesbrand
+                            Design & Develop by Maroon Lab.
                         </div>
                     </div>
                 </div>
@@ -99,36 +97,7 @@
         </div>
 
         <!-- Settings -->
-        <hr class="mt-0" />
-        <h6 class="text-center mb-0">Choose Layouts</h6>
-
-        <div class="p-4">
-            <div class="mb-2">
-                <img src="assets/images/layouts/layout-1.jpg" class="img-fluid img-thumbnail" alt="">
-            </div>
-            <div class="custom-control custom-switch mb-3">
-                <input type="checkbox" class="custom-control-input theme-choice" id="light-mode-switch" checked />
-                <label class="custom-control-label" for="light-mode-switch">Light Mode</label>
-            </div>
-
-            <div class="mb-2">
-                <img src="assets/images/layouts/layout-2.jpg" class="img-fluid img-thumbnail" alt="">
-            </div>
-            <div class="custom-control custom-switch mb-3">
-                <input type="checkbox" class="custom-control-input theme-choice" id="dark-mode-switch" data-bsStyle="assets/css/bootstrap-dark.min.css" data-appStyle="assets/css/app-dark.min.css" />
-                <label class="custom-control-label" for="dark-mode-switch">Dark Mode</label>
-            </div>
-
-            <div class="mb-2">
-                <img src="assets/images/layouts/layout-3.jpg" class="img-fluid img-thumbnail" alt="">
-            </div>
-            <div class="custom-control custom-switch mb-5">
-                <input type="checkbox" class="custom-control-input theme-choice" id="rtl-mode-switch" data-appStyle="assets/css/app-rtl.min.css" />
-                <label class="custom-control-label" for="rtl-mode-switch">RTL Mode</label>
-            </div>
-
-
-        </div>
+        @include('partials.settings')
 
     </div> <!-- end slimscroll-menu-->
 </div>
